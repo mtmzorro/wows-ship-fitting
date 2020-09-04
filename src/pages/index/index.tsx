ec1138a07aa5a8992fc217492c75058c48e40aaa
mtmzorro <mtmzorro@gmail.com>
2020-09-04 13:33:46 +0800

    Add LearnCloud connect

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { View, Button, Text, Image } from "@tarojs/components";
-import Taro from "@tarojs/taro";
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { View, Button, Text, Image } from '@tarojs/components'
+import Taro from '@tarojs/taro'
 
-import Fitting from "../fitting/fitting";
-import { Ship } from "../../type/types";
-import { getShips } from "../../service/ship";
-import { shipNameLocalize } from "../../utils/localization";
+import Fitting from '../fitting/fitting'
+import { Ship } from '../../type/types'
+import { getShips } from '../../service/ship'
+import { shipNameLocalize } from '../../utils/localization'
+import { queryAllFitting } from '../../service/common'
 
-
-import "./index.scss";
+import './index.scss'
 
 // #region 书写注意
 //
@@ -26,30 +26,37 @@ type State = {
 }
 
 interface Index {
-    state: State;
+    state: State
 }
 
 class Index extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            shipData: []
+            shipData: [],
         }
     }
-    
+
     componentWillMount() {
         this.setState({
-            shipData: getShips()
+            shipData: getShips(),
         })
     }
 
     componentWillReceiveProps(nextProps) {
-        console.log(this.props, nextProps);
+        console.log(this.props, nextProps)
     }
 
     componentWillUnmount() {}
 
-    componentDidShow() {}
+    componentDidShow() {
+        // 获取服务端 全部 Fitting
+        // queryAllFitting()
+        //     .then((result) => {
+        //         console.log(result)
+        //     })
+        //     .catch((result) => {})
+    }
 
     componentDidHide() {}
 
@@ -63,7 +70,7 @@ class Index extends Component {
 
     buttonClickHandle() {
         Taro.navigateTo({
-            url: '/pages/fittingEditor/fittingEditor'
+            url: '/pages/fittingEditor/fittingEditor',
         })
     }
 
@@ -72,12 +79,8 @@ class Index extends Component {
             <View className='index'>
                 <View>
                     <Text>Hello, World</Text>
-                    <View>
-                        ID: {this.state.shipData[0].id}
-                    </View>
-                    <View>
-                        Name: {shipNameLocalize(this.state.shipData[0].id)}
-                    </View>
+                    <View>ID: {this.state.shipData[0].id}</View>
+                    <View>Name: {shipNameLocalize(this.state.shipData[0].id)}</View>
                     <Text>国家: {this.state.shipData[0].nation}</Text>
                     <Text>T: {this.state.shipData[0].tier}</Text>
                     <Image src={this.getShipImg(this.state.shipData[0].id)}></Image>
@@ -85,13 +88,18 @@ class Index extends Component {
                 <View>
                     <View>舰长: Azur_Belfast</View>
                     <View>
-                        <Image src={this.getCommanderImg({name: 'Azur_Belfast', nation: 'United_Kingdom'})}></Image>
+                        <Image
+                            src={this.getCommanderImg({
+                                name: 'Azur_Belfast',
+                                nation: 'United_Kingdom',
+                            })}
+                        ></Image>
                     </View>
                 </View>
                 <Button onClick={this.buttonClickHandle}>创建我的配船方案</Button>
             </View>
-        );
+        )
     }
 }
 
-export default Index;
+export default Index
--- src/service/common.ts
+++ src/service/common.ts
@@ -1,5 +1,19 @@
+import AV from 'leancloud-storage/dist/av-weapp.js'
 import { Fitting } from '../type/types'
+import LearnCloud from '../utils/learnCloud'
 
-export const saveFitting = (fitting: Fitting) => {
-    
+export const saveFitting = (fitting: Fitting): Promise<any> => {
+    return LearnCloud.save('Fitting', fitting)
+    // return new Promise((resolve) => {
+    //     learnCloud.save('Fitting', fitting).then((result) => {
+    //         console.log('saveFitting save success')
+    //         resolve(result)
+    //     }).catch((error) => {
+    //         console.log('saveFitting save error', error)
+    //     })
+    // })
+}
+
+export const queryAllFitting = (): Promise<any> => {
+    return LearnCloud.queryAll('Fitting')
 }
