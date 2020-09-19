996e09c0cf90a886969714017d2e8acd7b4b8e53
mtmzorro <mtmzorro@foxmail.com>
2020-09-20 00:11:26 +0800

    Update SkillSelector

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,6 +3,8 @@ const config = {
     language: 'zh-CN',
     CDNPath: 'https://jsfi',
     imageCDNPath: 'https://cdn.jsdelivr.net/gh/mtmzorro/ship-res@0.0.1',
+    shipImageCDNPath: 'https://cdn.jsdelivr.net/gh/mtmzorro/ship-res@0.0.1',
+    kitImageCDNPath: 'https://cdn.jsdelivr.net/gh/mtmzorro/ship-res-kit@0.0.1',
     leanCould: {
         appId: 'KaCFzqPM8xXss53Jnjag7aae-gzGzoHsz',
         appKey: 'YxRPcCLDwAbxuzlAog0YLhDC',
--- src/pages/skillSelector/skillItem/skillItem.tsx
+++ src/pages/skillSelector/skillItem/skillItem.tsx
@@ -1,4 +1,5 @@
 import React, { memo, useMemo } from 'react'
+import Taro from '@tarojs/taro'
 import { View, Button, Text, Image } from '@tarojs/components'
 import classnames from 'classnames'
 import {
@@ -7,30 +8,51 @@ import {
     skillPerksLocalize,
 } from '../../../utils/localization'
 import { SkillData } from '../skillSelector'
-import './skillItem.scss'
+import { getSkillImage } from '../../../service/skill'
 
 interface Props {
     skill: SkillData
     onSelect: (skill: SkillData) => void
 }
 
-const SkillItem: React.SFC<Props> = (props) => {
+const SkillItem: React.FC<Props> = (props) => {
     const { skill, onSelect } = props
     const classNames = classnames('skill-item', {
-        'skill-item-selected': skill.selected 
+        'skill-item--selected': skill.selected,
     })
 
-    return useMemo(() => (
-        <View className={classNames} onClick={() => { onSelect(skill) }}>
-            {console.log('skill-item render')}
-            <Text>
-                {skill.sort} - {skillNameLocalize(skill.id)}
-            </Text>
-            {/* <View>{skillDescriptionLocalize(skill.id)}</View>
-            <View>{skillPerksLocalize(skill.id)}</View> */}
-            <View>----</View>
-        </View>
-    ), [skill.selected])
+    const handleSkillPrek = (prek: string) => {
+        Taro.showModal({
+            title: '技能加成',
+            content: prek,
+            showCancel: false,
+        })
+    }
+
+    return useMemo(
+        () => (
+            <View
+                className={classNames}
+                onClick={onSelect.bind(this, skill)}
+                onLongPress={handleSkillPrek.bind(this, skillPerksLocalize(skill.id))}
+            >
+                <View className='skill-item__icon'>
+                    <Image className='icon__image' lazyLoad src={getSkillImage(skill.id)} />
+                    <View className='icon__sort'>{skill.sort}</View>
+                </View>
+                <View className='skill-item__content'>
+                    <View className='content__name'>{skillNameLocalize(skill.id)}</View>
+                    <View className='content__des'>
+                        <View className='content__des-text'>
+                            {skillDescriptionLocalize(skill.id)}
+                        </View>
+                        {/* <View className='content__des-prek'>{skillPerksLocalize(skill.id)}</View> */}
+                    </View>
+                </View>
+            </View>
+        ),
+        [skill.selected]
+    )
 }
 
 export default SkillItem
