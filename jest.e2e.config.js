30624693af17fbe10ddfac369cca7fa97d901353
mtmzorro <mtmzorro@foxmail.com>
2020-10-06 16:08:44 +0800

    Add E2E test helpers

---
+++ jest.e2e.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-commonjs */
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  rootDir: "./__test__/e2e",
+  // Indicates whether each individual test should be reported during the run
+  verbose: true,
+};
