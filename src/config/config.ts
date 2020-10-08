6a3bc8234eb2c2443ae96d46d5bb61263bdf5bcc
mtmzorro <mtmzorro@foxmail.com>
2020-10-09 00:02:58 +0800

    Update .travis.yml

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,5 @@
+import leancloudApp from '../../.travis/leancloudApp.json'
+
 const config = {
     dataVersion: '0.9.5',
     language: 'zh-CN',
@@ -6,8 +8,8 @@ const config = {
     shipImageCDNPath: 'https://cdn.jsdelivr.net/gh/mtmzorro/ship-res@0.0.1',
     kitImageCDNPath: 'https://cdn.jsdelivr.net/gh/mtmzorro/ship-res-kit@0.0.1',
     leanCould: {
-        appId: 'KaCFzqPM8xXss53Jnjag7aae-gzGzoHsz',
-        appKey: 'YxRPcCLDwAbxuzlAog0YLhDC',
+        appId: leancloudApp.appId,
+        appKey: leancloudApp.appKey,
         serverURL: 'https://avoscloud.com',
     },
     dbClasses:{
